d4c0f403311e8c4f80c25620ba17926d6fa4b865
Boluwatife2904 <sanvicola2000@gmail.com>
2023-03-13 20:03:20 +0100

    chore: implement the UI for our services section

---
+++ components/HomeServices.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+const services = [
+	{ title: "ui/ux design", content: "Saving World with Good Designs. Because your satisfaction is everything." },
+	{ title: "development", content: "Saving World with Good Designs. Because your satisfaction is everything." },
+	{ title: "digital marketing", content: "Saving World with Good Designs. Because your satisfaction is everything." },
+	{ title: "product branding", content: "Saving World with Good Designs. Because your satisfaction is everything." },
+];
+</script>
+
+<template>
+	<section class="container">
+		<div class="our-services grid">
+			<div class="our-services__left">
+				<p class="paragraph-2 text-uppercase text-light-grey">our services</p>
+				<h2 class="heading-2 text-black text-uppercase weight-700 satoshi-font our-services__header">come on, we will help you buIld your dream project.</h2>
+				<div class="our-services__image">
+					<img src="/our-services.png" alt="Our services image" />
+				</div>
+			</div>
+			<div class="our-services__right">
+				<HomeServicesItem v-for="service in services" :key="service.title" :service="service" />
+			</div>
+		</div>
+	</section>
+</template>
+
+<style lang="scss" scoped>
+.our-services {
+	padding: 7rem 0 2.8rem;
+	gap: 1.8rem 11rem;
+	border-bottom: 2px dashed #e7dfd7;
+
+	@media screen and (min-width: 992px) {
+		padding: 15rem 0 7rem;
+		grid-template-columns: repeat(2, 1fr);
+		// gap: 11rem;
+	}
+
+	&__header {
+		max-width: 30.5rem;
+		margin-bottom: 2.8rem;
+
+		@media screen and (min-width: 992px) {
+			max-width: 46.8rem;
+			margin-bottom: 7rem;
+		}
+	}
+
+	&__left {
+		@media screen and (min-width: 992px) {
+			order: 1;
+		}
+	}
+}
+</style>
---
+++ components/HomeServicesItem.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+interface Service {
+	title: string;
+	content: string;
+}
+
+interface Props {
+	service: Service;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+	<div class="service-item flex space-between">
+		<div class="service-item__left">
+			<h3 class="service-item__title heading-3 satoshi-font weight-700 text-uppercase">{{ service.title }}</h3>
+			<p class="paragraph-1 text-paragraph">{{ service.content }}</p>
+		</div>
+		<div class="service-item__right">
+			<a href="#" title="Learn More" class="text-black weight-600 paragraph-1">Learn More</a>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.service-item {
+	gap: 1.1rem;
+
+	&:not(:last-child) {
+		padding-bottom: 1.8rem;
+		margin-bottom: 1.8rem;
+		border-bottom: 1px solid #d8d0c7;
+
+		@media screen and (min-width: 600px) {
+			padding-bottom: 4.8rem;
+			margin-bottom: 4.8rem;
+		}
+	}
+
+	&__title {
+		margin-bottom: 0.4rem;
+
+		@media screen and (min-width: 600px) {
+			margin-bottom: 1.8rem;
+		}
+	}
+
+	&__right {
+		align-self: flex-end;
+		flex-shrink: 0;
+
+		@media screen and (min-width: 600px) {
+			min-width: 8.9rem;
+		}
+	}
+}
+</style>
