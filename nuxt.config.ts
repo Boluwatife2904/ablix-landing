af8d1e79c75ac84f1e70840fffb1003764b6213e
Boluwatife2904 <sanvicola2000@gmail.com>
2023-03-17 21:59:06 -1000

    chore: update meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+		head: {
+			htmlAttrs: {
+				lang: "en",
+			},
+			title: "Ablix Studio",
+			charset: "UTF-8",
+			viewport: "width=device-width, initial-scale=1.0",
+			meta: [
+				{ hid: "description", name: "theme_color", content: "#fe7e41" },
+				{ hid: "description", name: "description", content: "Saving World with Good Designs. Because your satisfaction is everything." },
+				{ hid: "og:description", name: "og:description", content: "Saving World with Good Designs. Because your satisfaction is everything." },
+				{ hid: "og:title", name: "og:title", content: "Ablix Studio" },
+				{ hid: "og:site_name", name: "og:site_name", content: "Ablix Studio" },
+				{ hid: "og:url", name: "og:url", content: "https://ablix-landing.vercel.app" },
+				{ hid: "og:type", name: "og:type", content: "website" },
+				{ hid: "og:image", name: "og:image", content: "https://res.cloudinary.com/juwon-tech/image/upload/v1678474560/shoppe-ogimage_vxpqo1.png" },
+			],
+		},
+	},
 	css: ["@/assets/scss/main.scss", "@/assets/font.scss"],
 });
