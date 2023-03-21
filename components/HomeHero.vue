de5abe0d05dc35843c652d44db35d915de8d4eb0
Boluwatife2904 <sanvicola2000@gmail.com>
2023-03-21 20:31:17 +0100

    chore: add the hero section heading texts

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -1,7 +1,36 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div></div>
+	<section class="container">
+		<div class="home-hero">
+			<div class="home-hero__top">
+				<small class="home-hero__subheading text-uppercase paragraph-1 text-light-grey">😎 GIVING YOU THE BEST DESIGNS</small>
+				<h1 class="text-uppercase heading-1 satoshi-font weight-700">saving world with good designs.</h1>
+			</div>
+			<div class="home-hero__bottom">
+				<div class="home-hero__left"></div>
+				<div class="home-hero__right"></div>
+			</div>
+		</div>
+	</section>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.home-hero {
+	&__subheading {
+		margin-bottom: 0.5rem;
+	}
+
+	&__top {
+		max-width: 35rem;
+
+		@media screen and (min-width: 600px) {
+			max-width: 50rem;
+		}
+
+		@media screen and (min-width: 992px) {
+			max-width: 74.8rem;
+		}
+	}
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
+    <HomeHero />
     <HomeServices />
 	<HomeRecentWorks />
 	<HomeProcess />
